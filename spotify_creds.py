08981ef9d727658351172d74e76a25078d8b429b
Anwesh Badapanda <anweshbadapanda@yahoo.in>
2021-08-22 17:17:32 +0530

    completed yt downloads

--- spotify_creds.py
+++ spotify_creds.py
@@ -1,4 +1,4 @@
-client_id = "fafd1bcd81d94bb596cd4bc54cc0e1e1"
-client_secret = "446ab70ddbce422880fdcc3a5f95b1dd"
-username = 'anweshthejerk'
-download_location = "C:\\Users\\Admin\\OneDrive\\Documents\\Rockstar Games\\GTA V\\User Music\\"
+client_id = "YOUR_CLIENT_ID"
+client_secret = "YOUR_CLIENT_SECRET"
+username = 'YOUR_USERNAME'
+download_location = "USER_MUSIC_DIRECTORY"
